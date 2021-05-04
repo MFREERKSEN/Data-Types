cea2a2510a75881b82980808dc8c55b05ead0e03
MFreerksen <>
2021-05-04 11:50:21 +0000

    notes

--- index.js
+++ index.js
@@ -1,2 +1,98 @@
-console.log(typeof "Hello, World");
-console.log(typeof 17);
+// // // //console.log(typeof "Hello, World");
+// // // //console.log(typeof 17);
+// // // //console.log 
+// // // //console.log('Bruce's beard');
+// // // //console.log(42000);
+// // // //console.log(42111);
+// // // //console.log(42,17,56,34,11,4.35,32);
+// // // //console.log(3.4, "hello", 45);
+// // // //
+
+// // // // console.log(Number("2345"));
+// // // // console.log(typeof Number("2345"))
+// // // // console.log(Number(17));
+
+// // // console.log(Number("2345"));
+// // // console.log(typeof Number ("2345"));
+// // // console.log(Number(17));
+// // // console.log(Number("23bottles"));
+
+// // // console.log(Number ('4','3'));
+
+// // // let programmingLanguage = "JavaScript";
+// // // console.log (programmingLanguage)
+
+// // // let message = "Hello, World!";
+// // // console.log(message);
+
+
+
+
+
+// // // let message = "What's up, Doc?";
+// // // let n = 17;
+// // // let pi = 3.14159;
+
+// // // console.log(typeof message);
+// // // console.log(typeof n);
+// // // console.log(typeof pi);
+
+// // // console.log (message);
+// // // console.log (n);
+// // // console.log (pi);
+
+// // // let day = "Thursday";
+// // // console.log(day);
+
+// // // day = "Friday";
+// // // console.log(day);
+
+// // // day=21;
+// // // console.log(day);
+
+// // let day = "Thursday";
+// // day = 32.5;
+// // day = 19;
+// // console.log(day);
+
+
+
+// // const appName = "Get It Done!";
+// // appName = "Best TODO application EVER!";
+
+// // let sum = 1+2;
+// // console.log (sum);
+
+
+// // let minutes = 645;
+// // let hours = minutes / 60;
+// // console.log(hours);
+
+// // 16 - 2 * 5 / 3 +1 
+
+// // console.log(1 + 5 % 3)
+
+// // 2 ** 2 ** 3 * 3
+
+// // "Launch" + "Code"
+
+// const input = require ('readline-sync');
+
+// let name = input.question("Question text... ");
+
+// console.log (typeof info);
+// console.log("Hello, " + name + "!");
+
+// const input = require('readline-sync');
+
+// let num1 = input.question("Enter a number: ");
+// let num2 = input.question("Enter another number: ");
+
+// console.log (Number (num1) + Number (num2));
+
+console.log("Coding Rocks");
+console.log('Coding Rocks')
+// console.log(Coding Rocks);
+// console.log(Coding Rocks)
+console.log("Coding Rocks');
+
